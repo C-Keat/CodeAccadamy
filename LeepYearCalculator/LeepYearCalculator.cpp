d5d70c0cffa482450ed9275fe2a4f897290b8411
C-Keat <callumkeating86@gmail.com>
2021-07-01 13:03:59 +0100

    Start of the leap year challenge
    Research to be done to create this

---
+++ LeepYearCalculator/LeepYearCalculator.cpp
@@ -0,0 +1,37 @@
+// LeepYearCalculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+//Optional: Let’s try a challenging problem that will put all your brain cells to the test.
+
+//Write a leap_year.cpp program that :
+
+//Takes a year as input.
+//Checks to see if the year is a four - digit number.
+//Displays whether or not the year falls on a leap year.
+
+//There are 3 criteria that must be taken into account to identify a leap year :
+
+    //If the year can be evenly divided by 4 then it is a leap year, however…
+    //If that year can be evenly divided by 100, and it is not evenly divided by 400, then it is NOT a leap year.
+    //If that year is evenly divisible by 400, then it is a leap year.
+//Take some time to researchand brainstorm before starting to write the code!
+
+#include <iostream>
+
+int main()
+{
+
+    //set a year variable
+    //get input to see what year it is cin
+
+    //check to see if it is a 4 digit number - research how .size?
+
+    //calculate if the year is a leap year - research how to do this
+
+    //display if this year is a leap year
+
+
+
+    std::cout << "Hello World!\n";
+}
+
+
