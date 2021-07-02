f61b459167f732b8b043094db7f4590bd475073f
C-Keat <callumkeating86@gmail.com>
2021-07-02 09:50:04 +0100

    Comment changes
    Adding the step definitions

--- LeepYearCalculator/LeepYearCalculator.cpp
+++ LeepYearCalculator/LeepYearCalculator.cpp
@@ -46,7 +46,11 @@ int main()
 
     } while (stringLength != 4);
     
-
+    //1. If the year is evenly divisible by 4, go to step 2. Otherwise, go to step 5.
+    //2. If the year is evenly divisible by 100, go to step 3. Otherwise, go to step 4.
+    //3. If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
+    //4. The year is a leap year(it has 366 days).
+    //5. The year is not a leap year(it has 365 days).
     
     //calculate if the year is a leap year
     //display if this year is a leap year
